3de3cade1236cb955b7d22c4fa1f8f88015d6789
Aman Kumar <006amanraj@gmail.com>
2025-07-02 07:08:50 +0000

    what more enhancement we should do to make the exercise and lesson gener

--- src/components/lessons/adaptive-exercise.tsx
+++ src/components/lessons/adaptive-exercise.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Label } from "@/components/ui/label";
