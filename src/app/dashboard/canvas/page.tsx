c568a0ac94316542fb03c0d4a93c69917ce639d9
Aman Kumar <006amanraj@gmail.com>
2025-07-04 22:27:33 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button>
    
    with the

--- src/app/dashboard/canvas/page.tsx
+++ src/app/dashboard/canvas/page.tsx
@@ -64,7 +64,7 @@ const components = {
     <div style={{
       position: 'absolute',
       top: '10px',
-      right: '10px',
+      left: '10px',
       zIndex: 999,
       display: 'flex',
       gap: '10px'
